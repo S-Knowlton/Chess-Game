8a8bf49ce395c43607cb170e8f48c30757ac721f
KyleHersey <herseykw@plu.edu>
2015-02-14 13:04:01 -0800

    Located main entry point

--- ChessGame/ChessGame/MainWindow.xaml.cs
+++ ChessGame/ChessGame/MainWindow.xaml.cs
@@ -18,11 +18,15 @@ namespace ChessGame
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            //start game logic here
+
         }
     }
 }
