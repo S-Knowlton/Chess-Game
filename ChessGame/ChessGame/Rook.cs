94a8ee9ed29bfd0427567ef177b5676a1adfb12e
KyleHersey <herseykw@plu.edu>
2015-02-14 22:17:14 -0800

    Added Rook logic

--- ChessGame/ChessGame/Rook.cs
+++ ChessGame/ChessGame/Rook.cs
@@ -18,7 +18,87 @@ namespace ChessGame
 
         public override List<Point> getPossibleEndSpaces(Board b)
         {
-            return null;
+            List<Point> endSpaces = new List<Point>();
+
+            bool hitSomething = false;
+            Point currentPosition = new Point(position.X, position.Y);
+
+            //up
+            while(b.pointExists(new Point(position.X - 1, position.Y)) && hitSomething == false){
+                currentPosition.X -= 1;
+                if (b.GetPieceAt(currentPosition).player.GetID() == 0)
+                {
+                    endSpaces.Add(new Point(currentPosition.X, currentPosition.Y));
+                }
+                else if (b.GetPieceAt(currentPosition).player.GetID() != player.GetID())
+                {
+                    endSpaces.Add(new Point(currentPosition.X, currentPosition.Y));
+                    hitSomething = true;
+                }
+                else
+                {
+                    hitSomething = true;
+                }
+            }
+
+            //right
+            while (b.pointExists(new Point(position.X, position.Y + 1)) && hitSomething == false)
+            {
+                currentPosition.Y += 1;
+                if (b.GetPieceAt(currentPosition).player.GetID() == 0)
+                {
+                    endSpaces.Add(new Point(currentPosition.X, currentPosition.Y));
+                }
+                else if (b.GetPieceAt(currentPosition).player.GetID() != player.GetID())
+                {
+                    endSpaces.Add(new Point(currentPosition.X, currentPosition.Y));
+                    hitSomething = true;
+                }
+                else
+                {
+                    hitSomething = true;
+                }
+            }
+
+            //down
+            while (b.pointExists(new Point(position.X + 1, position.Y)) && hitSomething == false)
+            {
+                currentPosition.X += 1;
+                if (b.GetPieceAt(currentPosition).player.GetID() == 0)
+                {
+                    endSpaces.Add(new Point(currentPosition.X, currentPosition.Y));
+                }
+                else if (b.GetPieceAt(currentPosition).player.GetID() != player.GetID())
+                {
+                    endSpaces.Add(new Point(currentPosition.X, currentPosition.Y));
+                    hitSomething = true;
+                }
+                else
+                {
+                    hitSomething = true;
+                }
+            }
+
+            //left
+            while (b.pointExists(new Point(position.X, position.Y - 1)) && hitSomething == false)
+            {
+                currentPosition.Y -= 1;
+                if (b.GetPieceAt(currentPosition).player.GetID() == 0)
+                {
+                    endSpaces.Add(new Point(currentPosition.X, currentPosition.Y));
+                }
+                else if (b.GetPieceAt(currentPosition).player.GetID() != player.GetID())
+                {
+                    endSpaces.Add(new Point(currentPosition.X, currentPosition.Y));
+                    hitSomething = true;
+                }
+                else
+                {
+                    hitSomething = true;
+                }
+            }
+
+            return endSpaces;
         }
 
         public override string ToString(){
