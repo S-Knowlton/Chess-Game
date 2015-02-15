55aea6ae0fabb68d6b8883b9a17f8e15ff79a48c
KyleHersey <herseykw@plu.edu>
2015-02-14 20:58:47 -0800

    Added end spaces for King

--- ChessGame/ChessGame/King.cs
+++ ChessGame/ChessGame/King.cs
@@ -17,7 +17,36 @@ namespace ChessGame
 
         public override List<Point> getPossibleEndSpaces(Board b)
         {
-            return null;
+            List<Point> endSpaces = new List<Point>();
+
+            Point upLeft = new Point(position.X - 1, position.Y - 1);
+            Point up = new Point(position.X - 1, position.Y);
+            Point upRight = new Point(position.X - 1, position.Y + 1);
+            Point right = new Point(position.X, position.Y + 1);
+            Point downRight = new Point(position.X + 1, position.Y + 1);
+            Point down = new Point(position.X + 1, position.Y);
+            Point downLeft = new Point(position.X + 1, position.Y - 1);
+            Point left = new Point(position.X, position.Y - 1);
+
+            List<Point> possibles = new List<Point>();
+            possibles.Add(upLeft);
+            possibles.Add(up);
+            possibles.Add(upRight);
+            possibles.Add(right);
+            possibles.Add(downRight);
+            possibles.Add(down);
+            possibles.Add(downLeft);
+            possibles.Add(left);
+
+            for (int i = 0; i < possibles.Count(); i++)
+            {
+                if (b.pointExists(possibles[i]))
+                {
+                    endSpaces.Add(possibles[i]);
+                }
+            }
+
+            return endSpaces;
         }
 
         public override string ToString()
